3f8fdc924a3a81172b10cd6d80f1aed14c370abe
Cuaies <61624423+Cuaies@users.noreply.github.com>
2022-12-03 15:19:48 +0200

    feat: create `isAresCommand` function

--- src/util/helpers/stringUtil.ts
+++ src/util/helpers/stringUtil.ts
@@ -1,5 +1,17 @@
 import { Events } from "discord.js";
+import { AresChatInputCommand } from "../../modules/commands/aresChatInputCommand";
+import { AresApplicationCommandType } from "../../ts/types/types";
 
 export const isEventType = (str: string): str is keyof typeof Events => {
   return Object.values(Events).includes(str as Events);
+};
+
+export const isAresCommand = (
+  command: unknown
+): command is AresApplicationCommandType => {
+  if (!command || command.constructor === Array) return false;
+  if (command instanceof AresChatInputCommand) return true;
+  if (command instanceof AresChatInputCommand) return true;
+  if (command instanceof AresChatInputCommand) return true;
+  return false;
 };
