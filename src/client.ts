8e63fe56596bbc8c9be4334e4a8f2b75f2ea4b88
Cuaies <61624423+Cuaies@users.noreply.github.com>
2022-12-03 16:54:25 +0200

    feat: create manager base logic

--- src/client.ts
+++ src/client.ts
@@ -5,23 +5,28 @@ import { ClientConfig } from "./ts/interfaces/config.interface";
 import { AresEventManager } from "./modules/events/aresEventManager";
 import path from "path";
 import { LoggerScopes } from "./util/loggerScopes";
+import { AresCommandManager } from "./modules/commands/aresCommandManager";
 const { token, options }: ClientConfig = config.get("clientConfig");
 
-/**
- * Path to the event handlers' directory.
- */
+/** Path to the event handlers' directory. */
 const EVENTS_PATH = path.join(__dirname, "modules/events/handlers");
+/** Path to the commands' directory. */
+const COMMANDS_PATH = path.join(__dirname, "modules/commands/commands");
+
 export class AresClient extends Client {
   public eventManager: AresEventManager;
+  public commandManager: AresCommandManager;
 
   constructor(opts: ClientOptions) {
     super(opts);
 
     this.eventManager = new AresEventManager(this);
+    this.commandManager = new AresCommandManager(this);
   }
 
   async init() {
     this.eventManager.loadAll(EVENTS_PATH);
+    await this.commandManager.load(COMMANDS_PATH);
 
     logger.verbose(
       "[%s:%s] Client attempting to login",
---
+++ src/modules/commands/aresCommandManager.ts
@@ -0,0 +1,89 @@
+import { Collection } from "discord.js";
+import { readdir } from "fs/promises";
+import path from "path";
+import { AresClient } from "../../client";
+import {
+  AresApplicationCommandType,
+  CommandCollection,
+} from "../../ts/types/types";
+import { isAresCommand } from "../../util/helpers/stringUtil";
+import { LoggerScopes } from "../../util/loggerScopes";
+import logger from "../logger/logger";
+import CommandManagerResults from "./results";
+
+export class AresCommandManager {
+  client?: AresClient;
+  private _commands: CommandCollection;
+
+  constructor(client?: AresClient) {
+    this.client = client;
+    this._commands = new Collection();
+  }
+
+  get commands() {
+    return this._commands;
+  }
+
+  /**
+   * Loads command files.
+   * @param directory Path to the commands' directory.
+   */
+  public async load(directory: string): Promise<void> {
+    logger.verbose("[%s] Loading commands", LoggerScopes.CommandsManager);
+    const results = new CommandManagerResults();
+
+    const commandTypeDirs = await readdir(directory, {
+      withFileTypes: false,
+    });
+    if (!commandTypeDirs || !commandTypeDirs.length)
+      return results.displayResults();
+
+    // Loops through the command type directories.
+    await commandTypeDirs.reduce(async (p, typeDir) => {
+      await p;
+      const commandCategoryDirsPath = path.join(directory, typeDir);
+      const commandCategoryDirs = await readdir(commandCategoryDirsPath);
+      if (!commandCategoryDirs || !commandCategoryDirs.length) return;
+
+      // Loops through each command category directory found.
+      await commandCategoryDirs.reduce(async (p2, categoryDir) => {
+        await p2;
+        const commandFilesPath = path.join(
+          commandCategoryDirsPath,
+          categoryDir
+        );
+        const commandFiles = await readdir(commandFilesPath);
+        if (!commandFiles || !commandFiles.length) return;
+
+        // Loads command files from within the current looping directory.
+        await commandFiles.reduce(async (p3, file) => {
+          await p3;
+          const pathToCommand = path.join(commandFilesPath, file);
+          const command: AresApplicationCommandType = (
+            await import(pathToCommand)
+          ).default;
+          logger.debug(LoggerScopes.CommandsManager, {
+            iterating: command.name,
+            path: pathToCommand,
+          });
+
+          if (isAresCommand(command)) {
+            command.disabled
+              ? results.addDisabled(command)
+              : results.addCached(command);
+
+            this._commands.set(command.name, command);
+          } else {
+            logger.warn(
+              "[%s] Argument is not a command: %s",
+              LoggerScopes.CommandsManager,
+              command
+            );
+            results.addUncached(command);
+          }
+        }, Promise.resolve());
+      }, Promise.resolve());
+    }, Promise.resolve());
+    results.displayResults();
+  }
+}
