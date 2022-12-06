a83dc36a6bc749d3e413d63f3d368526cdbfe61c
Cuaies <61624423+Cuaies@users.noreply.github.com>
2022-12-06 14:36:33 +0200

    fix(lib): remove unused keys & fix success getter

--- src/lib/classes/baseResults.ts
+++ src/lib/classes/baseResults.ts
@@ -2,23 +2,18 @@ export abstract class BaseResults<T> {
   protected _cached: T[];
   protected _uncached: T[];
   protected _disabled: T[];
-  protected _success: boolean;
 
   constructor() {
     this._cached = [];
     this._uncached = [];
     this._disabled = [];
-    this._success = false;
   }
 
   get success() {
-    if (this._cached.length) {
-      if (this._uncached.length) {
-        return false;
-      }
-      return true;
+    if (this._uncached.length) {
+      return false;
     }
-    return false;
+    return true;
   }
 
   addCached(entry: T) {
