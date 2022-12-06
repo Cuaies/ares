275d18d1e197c2cc2736c1087e64ab2dda0be28b
Cuaies <61624423+Cuaies@users.noreply.github.com>
2022-12-06 16:03:29 +0200

    feat(config): add `clientId` and `supportGuild` entries

--- src/config/default.js
+++ src/config/default.js
@@ -4,9 +4,13 @@ const { Partials, GatewayIntentBits } = require("discord.js");
 module.exports = {
   clientConfig: {
     token: null,
+    clientId: null,
     options: {
       partials: [Partials.User],
       intents: [GatewayIntentBits.Guilds],
     },
+  },
+  supportGuild: {
+    guildId: null,
   },
 };
